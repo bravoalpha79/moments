50c0203830b02efae1bee65b6f140d46f63f1e93
User <user@codeinstitute.net>
2025-02-03 17:36:33 +0100

    Add whitenoise and modify URLs

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://drf-api-ba79-ab0b6297db4e.herokuapp.com/";
+axios.defaults.baseURL = "api/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
